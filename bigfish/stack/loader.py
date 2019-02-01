330d3af8544e5a7faac78ee9d9db0c42d54b7314
Arthur Imbert <arthur.imbert.pro@gmail.com>
2019-02-01 17:56:38 +0100

    create new submodules

---
+++ bigfish/stack/loader.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+Function used to read data from various sources and store them in a
+multidimensional tensor (technically a numpy ndarray).
+"""
+
+import os
+
+import numpy as np
+
+
---
+++ bigfish/stack/preprocessing.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+"""
+Functions used to format any input tensor loaded in bigfish.
+"""
