080936068783aaa71be445beb7d6ecd5dde9b16b
Arthur Imbert <arthur.imbert.pro@gmail.com>
2019-02-06 17:00:17 +0100

    add 2D plot functions

--- bigfish/plot/__init__.py
+++ bigfish/plot/__init__.py
@@ -4,7 +4,8 @@
 The bigfish.plot module includes function to plot images and simulated data.
 """
 
-from .plot_images import plot_yx
+from .plot_images import plot_yx, plot_channels_2d
 
 
-__all__ = ["plot_yx"]
+__all__ = ["plot_yx",
+           "plot_channels_2d"]
