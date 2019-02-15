e78d54fbf8ab419431ed89c6f407a53d66ace63c
Arthur Imbert <arthur.imbert.pro@gmail.com>
2019-02-15 13:26:29 +0100

    add projection plot

--- bigfish/plot/__init__.py
+++ bigfish/plot/__init__.py
@@ -4,9 +4,11 @@
 The bigfish.plot module includes function to plot images and simulated data.
 """
 
-from .plot_images import plot_yx, plot_channels_2d, plot_segmentation
+from .plot_images import (plot_yx, plot_channels_2d, plot_segmentation,
+                          plot_projection)
 
 
 __all__ = ["plot_yx",
            "plot_channels_2d",
+           "plot_projection",
            "plot_segmentation"]
--- bigfish/plot/plot_images.py
+++ bigfish/plot/plot_images.py
@@ -10,13 +10,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+# TODO add title in the plot and remove axes
+
 def plot_yx(tensor, r=0, c=0, z=0, title=None, framesize=(15, 15),
             path_output=None, ext="png"):
     """Plot the selected x and y dimensions of an image.
 
     Parameters
     ----------
-    tensor : np.ndarray, np.uint8
+    tensor : np.ndarray, np.uint
         A 2-d, 3-d or 5-d tensor with shape (y, x), (z, y, x) or
         (r, c, z, y, x) respectively.
     r : int
@@ -39,16 +41,17 @@ def plot_yx(tensor, r=0, c=0, z=0, title=None, framesize=(15, 15),
     -------
 
     """
+    # check tensor
+    stack.check_array(tensor, ndim=[2, 3, 5], dtype=[np.uint8, np.uint16])
+
     # get the 2-d tensor
+    xy_tensor = None
     if tensor.ndim == 2:
         xy_tensor = tensor
     elif tensor.ndim == 3:
         xy_tensor = tensor[z, :, :]
     elif tensor.ndim == 5:
         xy_tensor = tensor[r, c, z, :, :]
-    else:
-        raise ValueError("{0} is not a valid shape for the tensor."
-                         .format(tensor.shape))
 
     # plot
     plt.figure(figsize=framesize)
@@ -79,7 +82,7 @@ def plot_channels_2d(tensor, r=0, z=0, framesize=(15, 15), path_output=None,
 
     Parameters
     ----------
-    tensor : np.ndarray, np.uint8
+    tensor : np.ndarray, np.uint
         A 5-d tensor with shape (r, c, z, y, x).
     r : int
         Index of the round to keep.
@@ -98,7 +101,7 @@ def plot_channels_2d(tensor, r=0, z=0, framesize=(15, 15), path_output=None,
 
     """
     # check tensor
-    stack.check_array(tensor, ndim=5, dtype=np.uint8)
+    stack.check_array(tensor, ndim=5, dtype=[np.uint8, np.uint16])
 
     # get the number of channels
     nb_channels = tensor.shape[1]
@@ -122,6 +125,57 @@ def plot_channels_2d(tensor, r=0, z=0, framesize=(15, 15), path_output=None,
                     "{0}.".format(ext))
 
     return
+
+
+def plot_projection(tensor, projection, r=0, c=0, z=0, framesize=(15, 15),
+                    path_output=None, ext="png"):
+    """Plot result of a 2-d projection.
+
+    Parameters
+    ----------
+    tensor : np.ndarray, np.uint8
+        A 5-d tensor with shape (r, c, z, y, x).
+    projection : np.ndarray, np.uint8
+        A 2-d image with shape (y, x).
+    r : int
+        Index of the round to keep.
+    c : int
+        Index of the channel to keep.
+    z : int
+        Index of the z-slice to keep.
+    framesize : tuple
+        Size of the frame used to plot (plt.figure(figsize=framesize).
+    path_output : str
+        Path to save the image (without extension).
+    ext : str or List[str]
+        Extension used to save the plot. If it is a list of strings, the plot
+        will be saved several times.
+
+    Returns
+    -------
+
+    """
+    # check tensor
+    stack.check_array(tensor, ndim=5, dtype=np.uint8)
+    stack.check_array(projection, ndim=2, dtype=np.uint8)
+
+    # plot
+    fig, ax = plt.subplots(1, 2, sharex='col', figsize=framesize)
+    ax[0].imshow(tensor[r, c, z, :, :])
+    ax[1].imshow(projection)
+    plt.tight_layout()
+    plt.show()
+
+    # save the plot
+    if path_output is not None:
+        if isinstance(ext, str):
+            plt.savefig(path_output, format=ext)
+        elif isinstance(ext, list):
+            for ext_ in ext:
+                plt.savefig(path_output, format=ext_)
+        else:
+            Warning("Plot is not saved because the extension is not valid: "
+                    "{0}.".format(ext))
 
 
 def plot_segmentation(tensor, segmentation, r=0, c=0, z=0, label=None,
