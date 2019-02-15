955845398496c0f088caa4547b380b7da06a86be
Arthur Imbert <arthur.imbert.pro@gmail.com>
2019-02-15 12:29:12 +0100

    update __init__.py

--- bigfish/stack/__init__.py
+++ bigfish/stack/__init__.py
@@ -1,13 +1,29 @@
 # -*- coding: utf-8 -*-
 
 """
-The 'stack' module includes function to read data, preprocess them and build
-stack of images.
+The bigfish.stack module includes function to read data, preprocess them and
+build stack of images.
 """
 
-from .loader import read_tif, read_pickle, build_simulated_dataset
+from .loader import read_tif, read_pickle
+from .preprocess import (build_stack, check_recipe, build_simulated_dataset,
+                         projection, rescale, cast_uint8, cast_float32,
+                         log_filter, mean_filter, median_filter,
+                         maximum_filter, minimum_filter, load_stack)
 
 
 __all__ = ["read_tif",
            "read_pickle",
-           "build_simulated_dataset"]
+           "build_simulated_dataset",
+           "load_stack",
+           "build_stack",
+           "check_recipe",
+           "projection",
+           "rescale",
+           "cast_uint8",
+           "cast_float32",
+           "log_filter",
+           "mean_filter",
+           "median_filter",
+           "maximum_filter",
+           "minimum_filter"]
