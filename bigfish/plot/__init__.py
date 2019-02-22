136f5c5706122d55a5d4cfd8006ec12ad43fbb9b
Arthur Imbert <arthur.imbert.pro@gmail.com>
2019-02-22 08:46:33 +0100

    add projection parameter for spot detection plot

--- bigfish/plot/__init__.py
+++ bigfish/plot/__init__.py
@@ -5,11 +5,12 @@ The bigfish.plot module includes function to plot images and simulated data.
 """
 
 from .plot_images import (plot_yx, plot_channels_2d, plot_segmentation,
-                          plot_projection, plot_images)
+                          plot_projection, plot_images, plot_spot_detection)
 
 
 __all__ = ["plot_yx",
            "plot_images",
            "plot_channels_2d",
            "plot_projection",
-           "plot_segmentation"]
+           "plot_segmentation",
+           "plot_spot_detection"]
