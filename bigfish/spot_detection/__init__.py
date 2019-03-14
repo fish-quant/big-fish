159da82a4652c7b5caed3ac01869305a950c2018
Arthur Imbert <arthur.imbert.pro@gmail.com>
2019-03-14 18:24:21 +0100

    clean code detection

--- bigfish/spot_detection/__init__.py
+++ bigfish/spot_detection/__init__.py
@@ -5,9 +5,9 @@ The bigfish.detection module includes function to detect RNA spot in 2-d and
 3-d.
 """
 
-from .detection import detection, compute_snr, optimize_threshold_log_lm
+from .detection import (detection, compute_snr, get_sigma)
 
 
 __all__ = ["detection",
            "compute_snr",
-           "optimize_threshold_log_lm"]
+           "get_sigma"]
