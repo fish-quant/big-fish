1d2b891b626b7ea9777611031214e6305a8c8948
Arthur Imbert <arthur.imbert.pro@gmail.com>
2019-03-14 17:46:35 +0100

    add sanity checks

--- bigfish/stack/__init__.py
+++ bigfish/stack/__init__.py
@@ -7,10 +7,12 @@ build stack of images.
 
 from .loader import read_tif, read_pickle
 from .preprocess import (build_stack, check_recipe, build_simulated_dataset,
-                         projection, rescale, cast_uint8, cast_float32,
+                         projection, rescale, cast_img_uint8, cast_img_uint16,
                          log_filter, mean_filter, median_filter,
                          maximum_filter, minimum_filter, load_stack,
-                         gaussian_filter, build_stacks)
+                         gaussian_filter, build_stacks, cast_img_float32,
+                         cast_img_float64, compute_illumination_surface,
+                         correct_illumination_surface)
 from .utils import check_array, check_features_df
 
 
@@ -23,8 +25,10 @@ __all__ = ["read_tif",
            "check_recipe",
            "projection",
            "rescale",
-           "cast_uint8",
-           "cast_float32",
+           "cast_img_uint8",
+           "cast_img_uint16",
+           "cast_img_float32",
+           "cast_img_float64",
            "log_filter",
            "gaussian_filter",
            "mean_filter",
@@ -32,4 +36,6 @@ __all__ = ["read_tif",
            "maximum_filter",
            "minimum_filter",
            "check_array",
-           "check_features_df"]
+           "check_features_df",
+           "compute_illumination_surface",
+           "correct_illumination_surface"]
