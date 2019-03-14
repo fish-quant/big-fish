bdb4a2c42227dbafb2a7649484d64d5670571db6
Arthur Imbert <arthur.imbert.pro@gmail.com>
2019-03-14 18:47:16 +0100

    add plot illumination surface & manage subtitle

--- bigfish/plot/__init__.py
+++ bigfish/plot/__init__.py
@@ -5,7 +5,8 @@ The bigfish.plot module includes function to plot images and simulated data.
 """
 
 from .plot_images import (plot_yx, plot_channels_2d, plot_segmentation,
-                          plot_projection, plot_images, plot_spot_detection)
+                          plot_projection, plot_images, plot_spot_detection,
+                          plot_illumination_surface)
 
 
 __all__ = ["plot_yx",
@@ -13,4 +14,5 @@ __all__ = ["plot_yx",
            "plot_channels_2d",
            "plot_projection",
            "plot_segmentation",
-           "plot_spot_detection"]
+           "plot_spot_detection",
+           "plot_illumination_surface"]
--- bigfish/plot/plot_images.py
+++ bigfish/plot/plot_images.py
@@ -14,7 +14,7 @@ import numpy as np
 
 def plot_yx(tensor, r=0, c=0, z=0, title=None, framesize=(15, 15),
             path_output=None, ext="png"):
-    """Plot the selected x and y dimensions of an image.
+    """Plot the selected yx plan of the selected dimensions of an image.
 
     Parameters
     ----------
@@ -43,7 +43,9 @@ def plot_yx(tensor, r=0, c=0, z=0, title=None, framesize=(15, 15),
     """
     # check tensor
     stack.check_array(tensor, ndim=[2, 3, 5],
-                      dtype=[np.uint8, np.uint16, np.float32, bool])
+                      dtype=[np.uint8, np.uint16,
+                             np.float32, np.float64,
+                             bool])
 
     # get the 2-d tensor
     xy_tensor = None
@@ -61,15 +63,14 @@ def plot_yx(tensor, r=0, c=0, z=0, title=None, framesize=(15, 15),
         plt.title(title, fontweight="bold", fontsize=25)
     plt.axis('off')
     plt.tight_layout()
-    plt.show()
-
-    # save the plot
     _save_plot(path_output, ext)
+    plt.show()
 
     return
 
 
-def plot_images(images, framesize=(15, 15), path_output=None, ext="png"):
+def plot_images(images, framesize=(15, 15), titles=None,
+                path_output=None, ext="png"):
     """Plot or subplot of 2-d images.
 
     Parameters
@@ -78,6 +79,8 @@ def plot_images(images, framesize=(15, 15), path_output=None, ext="png"):
         Images with shape (y, x).
     framesize : tuple
         Size of the frame used to plot with 'plt.figure(figsize=framesize)'.
+    titles : List[str]
+        Titles of the subplots.
     path_output : str
         Path to save the image (without extension).
     ext : str or List[str]
@@ -104,7 +107,7 @@ def plot_images(images, framesize=(15, 15), path_output=None, ext="png"):
 
     # plot one image
     if len(images) == 1:
-        plot_yx(images[0], framesize=framesize,
+        plot_yx(images[0], framesize=framesize, title=titles,
                 path_output=path_output, ext=ext)
         return
 
@@ -113,21 +116,27 @@ def plot_images(images, framesize=(15, 15), path_output=None, ext="png"):
     if len(images) in [2, 3]:
         for i, image in enumerate(images):
             ax[i].imshow(image)
+            if titles is not None:
+                ax[i].set_title(titles[i], fontweight="bold", fontsize=15)
     else:
         for i, image in enumerate(images):
             row = i // 3
             col = i % 3
             ax[row, col].imshow(image)
+            if titles is not None:
+                ax[row, col].set_title(titles[i],
+                                       fontweight="bold", fontsize=15)
     plt.tight_layout()
+    _save_plot(path_output, ext)
     plt.show()
 
-    # save the plot
-    _save_plot(path_output, ext)
+    return
 
 
-def plot_channels_2d(tensor, r=0, z=0, framesize=(15, 15), path_output=None,
-                     ext="png"):
-    """Subplot the selected x and y dimensions of an image for all channels.
+def plot_channels_2d(tensor, r=0, z=0, framesize=(15, 15), titles=None,
+                     path_output=None, ext="png"):
+    """Subplot the yx plan of the selected dimensions of an image for all
+    channels.
 
     Parameters
     ----------
@@ -139,6 +148,8 @@ def plot_channels_2d(tensor, r=0, z=0, framesize=(15, 15), path_output=None,
         Index of the z slice to keep.
     framesize : tuple
         Size of the frame used to plot with 'plt.figure(figsize=framesize)'.
+    titles : List[str]
+        Titles of the subplots (one per channel).
     path_output : str
         Path to save the image (without extension).
     ext : str or List[str]
@@ -159,11 +170,58 @@ def plot_channels_2d(tensor, r=0, z=0, framesize=(15, 15), path_output=None,
     fig, ax = plt.subplots(1, nb_channels, sharex='col', figsize=framesize)
     for i in range(nb_channels):
         ax[i].imshow(tensor[r, i, z, :, :])
+        if titles is not None:
+            ax[i].set_title(titles[i], fontweight="bold", fontsize=15)
     plt.tight_layout()
+    _save_plot(path_output, ext)
     plt.show()
 
-    # save the plot
+    return
+
+
+def plot_illumination_surface(illumination_surface, r=0, framesize=(15, 15),
+                              titles=None, path_output=None, ext="png"):
+    """Subplot the yx plan of the dimensions of an illumination surface for
+    all channels.
+
+    Parameters
+    ----------
+    illumination_surface : np.ndarray, np.float
+        A 4-d tensor with shape (r, c, y, x) approximating the average
+        differential of illumination in our stack of images, for each channel
+        and each round.
+    r : int
+        Index of the round to keep.
+    framesize : tuple
+        Size of the frame used to plot with 'plt.figure(figsize=framesize)'.
+    titles : List[str]
+        Titles of the subplots (one per channel).
+    path_output : str
+        Path to save the image (without extension).
+    ext : str or List[str]
+        Extension used to save the plot. If it is a list of strings, the plot
+        will be saved several times.
+
+    Returns
+    -------
+
+    """
+    # check tensor
+    stack.check_array(illumination_surface, ndim=4,
+                      dtype=[np.float32, np.float64])
+
+    # get the number of channels
+    nb_channels = illumination_surface.shape[1]
+
+    # plot
+    fig, ax = plt.subplots(1, nb_channels, sharex='col', figsize=framesize)
+    for i in range(nb_channels):
+        ax[i].imshow(illumination_surface[r, i, :, :])
+        if titles is not None:
+            ax[i].set_title(titles[i], fontweight="bold", fontsize=15)
+    plt.tight_layout()
     _save_plot(path_output, ext)
+    plt.show()
 
     return
 
@@ -176,7 +234,7 @@ def plot_projection(tensor, projection, r=0, c=0, z=0, framesize=(15, 15),
     ----------
     tensor : np.ndarray, np.uint
         A 5-d tensor with shape (r, c, z, y, x).
-    projection : np.ndarray, np.uint8
+    projection : np.ndarray
         A 2-d image with shape (y, x).
     r : int
         Index of the round to keep.
@@ -198,22 +256,25 @@ def plot_projection(tensor, projection, r=0, c=0, z=0, framesize=(15, 15),
     """
     # check tensor
     stack.check_array(tensor, ndim=5, dtype=[np.uint8, np.uint16])
-    stack.check_array(projection, ndim=2, dtype=[np.uint8, np.uint16])
+    stack.check_array(projection, ndim=2, dtype=[np.uint8, np.uint16,
+                                                 np.float32, np.float64])
 
     # plot
     fig, ax = plt.subplots(1, 2, sharex='col', figsize=framesize)
     ax[0].imshow(tensor[r, c, z, :, :])
+    ax[0].set_title("Z-slice: {0}".format(z), fontweight="bold", fontsize=15)
     ax[1].imshow(projection)
+    ax[1].set_title("Projected image", fontweight="bold", fontsize=15)
     plt.tight_layout()
+    _save_plot(path_output, ext)
     plt.show()
 
-    # save the plot
-    _save_plot(path_output, ext)
+    return
 
 
 def plot_segmentation(tensor, segmentation, r=0, c=0, z=0, label=None,
                       framesize=(15, 15), path_output=None, ext="png"):
-    """Plot result of a 2-d segmentation, with labelled instances is available.
+    """Plot result of a 2-d segmentation, with labelled instances if available.
 
     Parameters
     ----------
@@ -251,17 +312,26 @@ def plot_segmentation(tensor, segmentation, r=0, c=0, z=0, label=None,
     if label is not None:
         fig, ax = plt.subplots(1, 3, sharex='col', figsize=framesize)
         ax[0].imshow(tensor[r, c, z, :, :])
+        ax[0].set_title("Z-slice: {0}".format(z),
+                        fontweight="bold", fontsize=15)
         ax[1].imshow(segmentation)
+        ax[1].set_title("Segmentation", fontweight="bold", fontsize=15)
         ax[2].imshow(label)
+        ax[2].set_title("Labels", fontweight="bold", fontsize=15)
+
     else:
         fig, ax = plt.subplots(1, 2, sharex='col', figsize=framesize)
         ax[0].imshow(tensor[r, c, z, :, :])
+        ax[0].set_title("Z-slice: {0}".format(z),
+                        fontweight="bold", fontsize=15)
         ax[1].imshow(segmentation)
+        ax[1].set_title("Segmentation", fontweight="bold", fontsize=15)
+
     plt.tight_layout()
+    _save_plot(path_output, ext)
     plt.show()
 
-    # save the plot
-    _save_plot(path_output, ext)
+    return
 
 
 def plot_spot_detection(tensor, coordinates, radius, r=0, c=0, z=0,
@@ -313,7 +383,9 @@ def plot_spot_detection(tensor, coordinates, radius, r=0, c=0, z=0,
         # plot
         fig, ax = plt.subplots(1, 2, figsize=framesize)
         ax[0].imshow(image_2d)
+        ax[1].set_title("Projected image", fontweight="bold", fontsize=15)
         ax[1].imshow(image_2d)
+        ax[1].set_title("All detected spots", fontweight="bold", fontsize=15)
         for spot_coordinate in coordinates:
             _, y, x = spot_coordinate
             c = plt.Circle((x, y), radius,
@@ -322,6 +394,7 @@ def plot_spot_detection(tensor, coordinates, radius, r=0, c=0, z=0,
                            fill=False)
             ax[1].add_patch(c)
         plt.tight_layout()
+        _save_plot(path_output, ext)
         plt.show()
 
     # a specific z-slice
@@ -336,7 +409,10 @@ def plot_spot_detection(tensor, coordinates, radius, r=0, c=0, z=0,
         # plot
         fig, ax = plt.subplots(1, 2, figsize=framesize)
         ax[0].imshow(image_2d)
+        ax[0].set_title("Z-slice: {0}".format(z),
+                        fontweight="bold", fontsize=15)
         ax[1].imshow(image_2d)
+        ax[1].set_title("Detected spots", fontweight="bold", fontsize=15)
         for spot_coordinate in coordinates:
             y, x = spot_coordinate
             c = plt.Circle((x, y), radius,
@@ -345,10 +421,10 @@ def plot_spot_detection(tensor, coordinates, radius, r=0, c=0, z=0,
                            fill=False)
             ax[1].add_patch(c)
         plt.tight_layout()
+        _save_plot(path_output, ext)
         plt.show()
 
-    # save the plot
-    _save_plot(path_output, ext)
+    return
 
 
 def _save_plot(path_output, ext):
@@ -376,3 +452,5 @@ def _save_plot(path_output, ext):
         else:
             Warning("Plot is not saved because the extension is not valid: "
                     "{0}.".format(ext))
+
+    return
