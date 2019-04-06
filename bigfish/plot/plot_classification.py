a43391554407ec1845546878156b95180e224fdc
Arthur Imbert <arthur.imbert.pro@gmail.com>
2019-04-06 16:27:30 +0200

    add size parameters for classification plots

--- bigfish/plot/plot_classification.py
+++ bigfish/plot/plot_classification.py
@@ -13,25 +13,8 @@ from sklearn.metrics import confusion_matrix
 
 def plot_confusion_matrix(y_true, y_pred, normalize=False, classes_num=None,
                           classes_str=None, title=None, framesize=(8, 8),
-                          path_output=None, ext="png"):
-    """
-
-    Parameters
-    ----------
-    y_true
-    y_pred
-    normalize
-    classes_num
-    classes_str
-    title
-    framesize
-    path_output
-    ext
-
-    Returns
-    -------
-
-    """
+                          size_title=20, size_axes=15, path_output=None,
+                          ext="png"):
     # TODO add documentation
     # compute confusion matrix
     cm = confusion_matrix(y_true=y_true, y_pred=y_pred, labels=classes_num)
@@ -44,16 +27,18 @@ def plot_confusion_matrix(y_true, y_pred, normalize=False, classes_num=None,
                        out=np.zeros_like(cm),
                        where=mask)
 
-    # plot confusion matrix and colorbar
+    # plot confusion matrix
     fig, ax = plt.subplots(figsize=framesize)
     frame = ax.imshow(cm, interpolation='nearest', cmap=plt.get_cmap("Blues"))
+
+    # colorbar
     colorbar = ax.figure.colorbar(frame, ax=ax, fraction=0.0453, pad=0.05)
     if normalize:
         colorbar.ax.set_ylabel("Density", rotation=-90, va="bottom",
-                               fontweight="bold", fontsize=10)
+                               fontweight="bold", fontsize=size_axes-5)
     else:
         colorbar.ax.set_ylabel("Frequency", rotation=-90, va="bottom",
-                               fontweight="bold", fontsize=10)
+                               fontweight="bold", fontsize=size_axes-5)
     # cax = divider.append_axes("right", size=width, pad=pad)
 
     # set ticks
@@ -65,22 +50,25 @@ def plot_confusion_matrix(y_true, y_pred, normalize=False, classes_num=None,
     ax.tick_params(which="minor", bottom=False, left=False)
     if classes_str is not None:
         ax.set_xticklabels(classes_str, rotation=45, ha="right",
-                           rotation_mode="anchor", fontsize=10)
-        ax.set_yticklabels(classes_str, fontsize=10)
+                           rotation_mode="anchor", fontsize=size_axes-5)
+        ax.set_yticklabels(classes_str, fontsize=size_axes-5)
+
+    # title and axes labels
     if title is not None:
-        ax.set_title(title, fontweight="bold", fontsize=20)
-    ax.set_xlabel("Predicted label", fontweight="bold", fontsize=15)
-    ax.set_ylabel("True label", fontweight="bold", fontsize=15)
+        ax.set_title(title, fontweight="bold", fontsize=size_title)
+    ax.set_xlabel("Predicted label", fontweight="bold", fontsize=size_axes)
+    ax.set_ylabel("True label", fontweight="bold", fontsize=size_axes)
 
     # text annotations in the matrix
     fmt = '.2f' if normalize else 'd'
     threshold = np.nanmax(cm) / 2.
     for i in range(cm.shape[0]):
         for j in range(cm.shape[1]):
-            ax.text(j, i, format(cm[i, j], fmt), fontsize=8,
+            ax.text(j, i, format(cm[i, j], fmt), fontsize=size_axes-7,
                     ha="center", va="center",
                     color="white" if cm[i, j] > threshold else "black")
 
+    # show frame
     fig.tight_layout()
     save_plot(path_output, ext)
     fig.show()
@@ -89,27 +77,9 @@ def plot_confusion_matrix(y_true, y_pred, normalize=False, classes_num=None,
 
 
 def plot_2d_projection(x, y, labels_num, labels_str, colors, markers=None,
-                       title=None, framesize=(8, 8), path_output=None,
-                       ext="png"):
-    """
-
-    Parameters
-    ----------
-    x
-    y
-    labels_num
-    labels_str
-    colors
-    markers
-    title
-    framesize
-    path_output
-    ext
-
-    Returns
-    -------
-
-    """
+                       title=None, framesize=(10, 10), size_data=50, alpha=0.8,
+                       size_title=20, size_axes=15, size_legend=15,
+                       path_output=None, ext="png"):
     # TODO add documentation
     # define markers
     if markers is None:
@@ -119,12 +89,17 @@ def plot_2d_projection(x, y, labels_num, labels_str, colors, markers=None,
     plt.figure(figsize=framesize)
     for i, label_num in enumerate(labels_num):
         plt.scatter(x[y == label_num, 0], x[y == label_num, 1],
-                    s=30, c=colors[i], label=labels_str[i], marker=markers[i])
+                    s=size_data, c=colors[i], label=labels_str[i],
+                    marker=markers[i], alpha=alpha)
+
+    # text annotations
     if title is not None:
-        plt.title(title, fontweight="bold", fontsize=20)
-    plt.xlabel("First component", fontweight="bold", fontsize=15)
-    plt.ylabel("Second component", fontweight="bold", fontsize=15)
-    plt.legend(prop={'size': 10})
+        plt.title(title, fontweight="bold", fontsize=size_title)
+    plt.xlabel("First component", fontweight="bold", fontsize=size_axes)
+    plt.ylabel("Second component", fontweight="bold", fontsize=size_axes)
+    plt.legend(prop={'size': size_legend})
+
+    # show frame
     plt.tight_layout()
     save_plot(path_output, ext)
     plt.show()
