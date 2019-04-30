3ff36f9535b5ef6ae420a3e9a5081bcb08646660
Arthur Imbert <arthur.imbert.pro@gmail.com>
2019-04-30 19:48:52 +0200

    improve sanity checks and loaders

--- bigfish/stack/utils.py
+++ bigfish/stack/utils.py
@@ -4,41 +4,116 @@
 Utility functions for bigfish.stack submodule.
 """
 
+import inspect
+import re
+
 import numpy as np
+import pandas as pd
 
 
-# TODO complete the checks for the dataframe (dtype, missing values).
-# ### Sanity checks ###
+# ### Sanity checks dataframe ###
 
-def check_features_df(df, features):
-    """Check that the dataframe has the right features.
+def check_df(df, features=None, features_nan=None):
+    """Full safety check of a dataframe.
 
     Parameters
     ----------
     df : pd.DataFrame
         Dataframe to check.
     features : List[str]
-        Names of the features expected.
+        Names of the expected features.
+    features_nan : List[str]
+        Names of the features to check for the missing values
+
+    Returns
+    -------
+    _ : bool
+        Assert if the dataframe is well formatted.
+
+    """
+    # check parameters
+    check_parameter(features=(list, type(None)),
+                    features_nan=(list, type(None)))
+
+    # check the dataframe itself
+    if not isinstance(df, pd.DataFrame):
+        raise ValueError("Data should be a pd.DataFrame instead of {0}."
+                         .format(type(df)))
+
+    # check features
+    if features is not None:
+        _check_features_df(df, features)
+
+    # check NaN values
+    if features_nan is not None:
+        _check_features_df(df, features_nan)
+        _check_nan_df(df, features_nan)
+
+    # TODO complete the checks for the dataframe (dtype).
+
+    return True
+
+
+def _check_features_df(df, features):
+    """Check that the dataframe contains expected features.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Dataframe to check.
+    features : List[str]
+        Names of the expected features.
 
     Returns
     -------
 
     """
-    # get dataframe's features
-    col_names = df.columns
+    # check columns
+    if not set(features).issubset(df.columns):
+        raise ValueError("The dataframe does not seem to have the right "
+                         "features. {0} instead of {1}"
+                         .format(df.columns, features))
 
-    # sort the two lists
-    col_names = sorted(col_names)
-    features = sorted(features)
+    return
 
-    if col_names == features:
-        return
+
+def _check_nan_df(df, features_nan=None):
+    """
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Dataframe to check.
+    features_nan : List[str]
+        Names of the checked features.
+
+    Returns
+    -------
+
+    """
+    # count NaN
+    nan_count = df.isnull().sum()
+
+    # for the full dataframe...
+    if features_nan is None:
+        x = nan_count.sum()
+        if x > 0:
+            raise ValueError("The dataframe has {0} NaN values.".format(x))
+
+    # ...or for some features
     else:
-        raise ValueError("The file does not seem to have the right features. "
-                         "{0} instead of {1}".format(col_names, features))
+        nan_count = nan_count[features_nan]
+        x = nan_count.sum()
+        if x > 0:
+            raise ValueError("The dataframe has {0} NaN values for the "
+                             "requested features: \n{1}.".format(x, nan_count))
+
+    return
 
 
-def check_array(array, ndim=None, dtype=None):
+# ### Sanity checks array ###
+
+def check_array(array, ndim=None, dtype=None, allow_nan=True):
     """Full safety check of an array.
 
     Parameters
@@ -49,15 +124,26 @@ def check_array(array, ndim=None, dtype=None):
         Number of dimensions expected.
     dtype : type or List[type]
         Types expected.
+    allow_nan : bool
+        Allow NaN values or not.
+    min_array : int
+        Minimum value allowed.
+    max_array : int
+        Maximum value allowed.
 
     Returns
     -------
+    _ : bool
+        Assert if the array is well formatted.
 
     """
-    # check the array itself
-    if not isinstance(array, np.ndarray):
-        raise ValueError("Data should be a np.ndarray instead of {0}."
-                         .format(type(array)))
+    # check parameters
+    check_parameter(array=np.ndarray,
+                    ndim=(int, list, type(None)),
+                    dtype=(type, type(None)),
+                    allow_nan=bool,
+                    min_array=(int, type(None)),
+                    max_array=(int, type(None)))
 
     # check the dtype
     if dtype is not None:
@@ -67,11 +153,11 @@ def check_array(array, ndim=None, dtype=None):
     if ndim is not None:
         _check_dim_array(array, ndim)
 
-    # TODO check the order of the dimensions
+    # check NaN
+    if not allow_nan:
+        _check_nan_array(array)
 
-    # TODO check nan
-
-    return
+    return True
 
 
 def _check_dtype_array(array, dtype):
@@ -114,7 +200,6 @@ def _check_dim_array(array, ndim):
     -------
 
     """
-
     # enlist the number of expected dimensions
     if isinstance(ndim, int):
         ndim = [ndim]
@@ -125,8 +210,29 @@ def _check_dim_array(array, ndim):
                          "dimensions are: {1}.".format(array.ndim, ndim))
 
 
-def check_range_value(array, min_, max_):
+def _check_nan_array(array):
+    """Check that the array does not have NaN values.
+
+    Parameters
+    ----------
+    array : np.ndarray
+        Array to check.
+
+    Returns
+    -------
+
     """
+    # count nan
+    mask = np.isnan(array)
+    x = mask.sum()
+
+    # check the NaN values of the array
+    if x > 0:
+        raise ValueError("Array has {0} NaN values.".format(x))
+
+
+def check_range_value(array, min_=None, max_=None):
+    """Check the support of the array.
 
     Parameters
     ----------
@@ -140,10 +246,97 @@ def check_range_value(array, min_, max_):
     Returns
     -------
     _ : bool
-        Assert if the array is within the requested bound.
+        Assert if the array has the right range of values.
 
     """
-    if array.min() < min_ or array.max() > max_:
-        return False
-    else:
-        return True
+    # check lowest and highest bounds
+    if min_ is not None and array.min() < min_:
+        raise ValueError("The array should have a lower bound of {0}, but its "
+                         "minimum value is {1}.".format(min_, array.min()))
+    if max_ is not None and array.max() > max_:
+        raise ValueError("The array should have an upper bound of {0}, but "
+                         "its maximum value is {1}.".format(max_, array.max()))
+
+    return True
+
+
+# ### Sanity checks parameters ###
+
+def check_recipe(recipe):
+    """Check and validate a recipe.
+
+    Checking a recipe consist in validating its filename pattern and the
+    content of the dictionary.
+
+    Parameters
+    ----------
+    recipe : dict
+        Map the images according to their field of view, their round,
+        their channel and their spatial dimensions. Can only contain the keys
+        'pattern', 'fov', 'r', 'c', 'z', 'ext' or 'opt'.
+
+    Returns
+    -------
+
+    """
+    # check recipe is a dictionary
+    if not isinstance(recipe, dict):
+        raise Exception("The recipe is not valid. It should be a dictionary.")
+
+    # check the filename pattern
+    if "pattern" not in recipe:
+        raise ValueError("A recipe should have a filename pattern "
+                         "('pattern' keyword).")
+    recipe_pattern = recipe["pattern"]
+    if not isinstance(recipe_pattern, str):
+        raise ValueError("'pattern' should be a string, not a {0}."
+                         .format(type(recipe_pattern)))
+
+    # count the different dimensions to combinate in the recipe (among
+    # 'fov', 'r', 'c' and 'z')
+    dimensions = re.findall("fov|r|c|z", recipe_pattern)
+
+    # each dimension can only appear once in the filename pattern
+    if len(dimensions) != len(set(dimensions)):
+        raise ValueError("The pattern used in recipe is wrong, a dimension "
+                         "appears several times: {0}".format(recipe_pattern))
+
+    # check keys and values of the recipe
+    for key, value in recipe.items():
+        if key not in ['fov', 'r', 'c', 'z', 'ext', 'opt']:
+            raise ValueError("The recipe can only contain the keys 'fov', "
+                             "'r', 'c', 'z', 'ext' or 'opt'. Not {0}."
+                             .format(key))
+        if not isinstance(value, (list, str)):
+            raise TypeError("A recipe can only contain lists or strings, "
+                            "not {0}.".format(type(value)))
+
+    return
+
+
+def check_parameter(**kwargs):
+    """Check dtype of the function's parameters.
+
+    Parameters
+    ----------
+    kwargs : dict
+        Map of each parameter with its expected dtype.
+
+    Returns
+    -------
+
+    """
+    # get the frame and the parameters of the function
+    frame = inspect.currentframe().f_back
+    _, _, _, values = inspect.getargvalues(frame)
+
+    # compare each parameter with its expected dtype
+    for arg in kwargs:
+        expected_dtype = kwargs[arg]
+        parameter = values[arg]
+        if not isinstance(parameter, expected_dtype):
+            raise ValueError("Parameter {0} should be cast in {1}. It is a {2}"
+                             "instead."
+                             .format(arg, expected_dtype, type(parameter)))
+
+    return
