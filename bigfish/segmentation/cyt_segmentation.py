f28364235e81481f9b9ec274887f8d696d79bb66
Arthur Imbert <arthur.imbert.pro@gmail.com>
2019-06-06 11:29:12 +0200

    improve normalization of watershed relief

--- bigfish/segmentation/cyt_segmentation.py
+++ bigfish/segmentation/cyt_segmentation.py
@@ -15,93 +15,6 @@ from skimage.measure import regionprops
 from scipy import ndimage as ndi
 
 
-# TODO rename functions
-
-
-def build_cyt_relief(image_projected, nuc_labelled, alpha=0.8):
-    """Compute a 2-d representation of the cytoplasm to be used by watershed
-    algorithm.
-
-    Cells are represented as watershed, with a low values to the center and
-    maximum values at their borders.
-
-    The equation used is:
-        relief = alpha * relief_pixel + (1 - alpha) * relief_distance
-
-    - 'relief_pixel' exploit the differences in pixel intensity values.
-    - 'relief_distance' use the distance from the nuclei.
-
-    Parameters
-    ----------
-    image_projected : np.ndarray, np.uint
-        Projected image of the cytoplasm with shape (y, x).
-    nuc_labelled : np.ndarray,
-        Result of the nuclei segmentation with shape (y, x).
-    alpha : float or int
-        Weight of the pixel intensity values to compute the relief. A value of
-        0 and 1 respectively return 'relief_distance' and 'relief_pixel'.
-
-    Returns
-    -------
-    relief : np.ndarray, np.uint
-        Relief image of the cytoplasm with shape (y, x).
-
-    """
-    # TODO use distance map from bigfish.stack
-    # check parameters
-    stack.check_array(image_projected,
-                      ndim=2,
-                      dtype=[np.uint8, np.uint16],
-                      allow_nan=False)
-    stack.check_array(nuc_labelled,
-                      ndim=2,
-                      dtype=[np.uint8, np.uint16, np.int64, bool],
-                      allow_nan=False)
-    stack.check_parameter(alpha=(float, int))
-
-    # use pixel intensity of the cytoplasm channel to compute the seed.
-    if alpha == 1:
-        relief = image_projected.copy()
-        max_intensity = np.iinfo(image_projected.dtype).max
-        relief = max_intensity - relief
-        relief[nuc_labelled > 0] = 0
-
-    # use distance from the nuclei
-    elif alpha == 0:
-        binary_mask_nuc = nuc_labelled > 0
-        relief = ndi.distance_transform_edt(~binary_mask_nuc)
-        relief = np.true_divide(relief, relief.max(), dtype=np.float32)
-        if image_projected.dtype == np.uint8:
-            relief = stack.cast_img_uint8(relief)
-        else:
-            relief = stack.cast_img_uint16(relief)
-
-    # use both previous methods
-    elif 0 < alpha < 1:
-        relief_pixel = image_projected.copy()
-        max_intensity = np.iinfo(image_projected.dtype).max
-        relief_pixel = max_intensity - relief_pixel
-        relief_pixel[nuc_labelled > 0] = 0
-        relief_pixel = stack.cast_img_float32(relief_pixel)
-        binary_mask_nuc = nuc_labelled > 0
-        relief_distance = ndi.distance_transform_edt(~binary_mask_nuc)
-        relief_distance = np.true_divide(relief_distance,
-                                         relief_distance.max(),
-                                         dtype=np.float32)
-        relief = alpha * relief_pixel + (1 - alpha) * relief_distance
-        if image_projected.dtype == np.uint8:
-            relief = stack.cast_img_uint8(relief)
-        else:
-            relief = stack.cast_img_uint16(relief)
-
-    else:
-        raise ValueError("Parameter 'alpha' is wrong. Must be comprised "
-                         "between 0 and 1. Currently 'alpha' is {0}"
-                         .format(alpha))
-
-    return relief
-
-
 def build_cyt_binary_mask(image_projected, threshold=None):
     """Compute a binary mask of the cytoplasm.
 
@@ -136,6 +49,101 @@ def build_cyt_binary_mask(image_projected, threshold=None):
     mask = remove_small_holes(mask, 2000)
 
     return mask
+
+
+def build_cyt_relief(image_projected, nuc_labelled, mask_cyt, alpha=0.8):
+    """Compute a 2-d representation of the cytoplasm to be used by watershed
+    algorithm.
+
+    Cells are represented as watershed, with a low values to the center and
+    maximum values at their borders.
+
+    The equation used is:
+        relief = alpha * relief_pixel + (1 - alpha) * relief_distance
+
+    - 'relief_pixel' exploit the differences in pixel intensity values.
+    - 'relief_distance' use the distance from the nuclei.
+
+    Parameters
+    ----------
+    image_projected : np.ndarray, np.uint
+        Projected image of the cytoplasm with shape (y, x).
+    nuc_labelled : np.ndarray,
+        Result of the nuclei segmentation with shape (y, x).
+    mask_cyt : np.ndarray, bool
+        Binary mask of the cytoplasm with shape (y, x).
+    alpha : float or int
+        Weight of the pixel intensity values to compute the relief. A value of
+        0 and 1 respectively return 'relief_distance' and 'relief_pixel'.
+
+    Returns
+    -------
+    relief : np.ndarray, np.uint
+        Relief image of the cytoplasm with shape (y, x).
+
+    """
+    # check parameters
+    stack.check_array(image_projected,
+                      ndim=2,
+                      dtype=[np.uint8, np.uint16],
+                      allow_nan=False)
+    stack.check_array(nuc_labelled,
+                      ndim=2,
+                      dtype=[np.uint8, np.uint16, np.int64, bool],
+                      allow_nan=False)
+    stack.check_array(mask_cyt,
+                      ndim=2,
+                      dtype=[bool],
+                      allow_nan=False)
+    stack.check_parameter(alpha=(float, int))
+
+    # use pixel intensity of the cytoplasm channel to compute the seed.
+    if alpha == 1:
+        relief = image_projected.copy()
+        max_intensity = np.iinfo(image_projected.dtype).max
+        relief = max_intensity - relief
+        relief[nuc_labelled > 0] = 0
+        relief[mask_cyt == 0] = max_intensity
+        relief = stack.rescale(relief)
+
+    # use distance from the nuclei
+    elif alpha == 0:
+        binary_mask_nuc = nuc_labelled > 0
+        relief = ndi.distance_transform_edt(~binary_mask_nuc)
+        relief[mask_cyt == 0] = relief.max()
+        relief = np.true_divide(relief, relief.max(), dtype=np.float32)
+        if image_projected.dtype == np.uint8:
+            relief = stack.cast_img_uint8(relief)
+        else:
+            relief = stack.cast_img_uint16(relief)
+
+    # use both previous methods
+    elif 0 < alpha < 1:
+        relief_pixel = image_projected.copy()
+        max_intensity = np.iinfo(image_projected.dtype).max
+        relief_pixel = max_intensity - relief_pixel
+        relief_pixel[nuc_labelled > 0] = 0
+        relief_pixel[mask_cyt == 0] = max_intensity
+        relief_pixel = stack.rescale(relief_pixel)
+        relief_pixel = stack.cast_img_float32(relief_pixel)
+        binary_mask_nuc = nuc_labelled > 0
+        relief_distance = ndi.distance_transform_edt(~binary_mask_nuc)
+        relief_distance[mask_cyt == 0] = relief_distance.max()
+        relief_distance = np.true_divide(relief_distance,
+                                         relief_distance.max(),
+                                         dtype=np.float32)
+        relief = alpha * relief_pixel + (1 - alpha) * relief_distance
+        if image_projected.dtype == np.uint8:
+            relief = stack.cast_img_uint8(relief)
+        else:
+            relief = stack.cast_img_uint16(relief)
+
+    else:
+        raise ValueError("Parameter 'alpha' is wrong. Must be comprised "
+                         "between 0 and 1. Currently 'alpha' is {0}"
+                         .format(alpha))
+
+    return relief
 
 
 def cyt_watershed(relief, nuc_labelled, mask):
