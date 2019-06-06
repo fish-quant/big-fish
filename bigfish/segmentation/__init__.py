84b9fff9daea6ae6190ba291d37b398c05eaa64e
Arthur Imbert <arthur.imbert.pro@gmail.com>
2019-06-06 14:11:44 +0200

    add 'get_boundaries'

--- bigfish/segmentation/__init__.py
+++ bigfish/segmentation/__init__.py
@@ -5,7 +5,8 @@ The bigfish.segmentation module includes function to segment nucleus,
 cytoplasm and label them, in 2-d and 3-d.
 """
 
-from .utils import label_instances, compute_mean_size_object, merge_labels
+from .utils import (label_instances, compute_mean_size_object, merge_labels,
+                    get_boundaries)
 from .nuc_segmentation import (filtered_threshold, remove_segmented_nuc)
 from .cyt_segmentation import (build_cyt_relief, build_cyt_binary_mask,
                                cyt_watershed)
@@ -17,6 +18,7 @@ _cyt = ["build_cyt_relief", "build_cyt_binary_mask", cyt_watershed]
 
 _unet = ["get_input_size_unet"]
 
-_utils = ["label_instances", "compute_mean_size_object", "merge_labels"]
+_utils = ["label_instances", "compute_mean_size_object", "merge_labels",
+          "get_boundaries"]
 
 __all__ = _utils + _nuc + _cyt
