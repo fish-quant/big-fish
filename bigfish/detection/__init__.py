4678703a5a0c88d6ad1d5dd3fde50d937873ac46
Arthur Imbert <arthur.imbert.pro@gmail.com>
2019-06-06 12:30:32 +0200

    build reference spot from detected spots

--- bigfish/detection/__init__.py
+++ bigfish/detection/__init__.py
@@ -5,17 +5,18 @@ The bigfish.detection module includes function to detect RNA spot in 2-d and
 3-d.
 """
 
-from .detection import (detection, compute_snr, get_sigma, detection_log_lm,
-                        detection_log_lm, log_lm, non_maximum_suppression_mask,
-                        from_threshold_to_spots, from_threshold_to_snr)
+from .spot_detection import (log_lm, local_maximum_detection,
+                             spots_thresholding, compute_snr,
+                             from_threshold_to_snr, get_sigma,
+                             build_reference_spot)
 from .gaussian_fit import (get_spot_volume, get_spot_surface, build_grid,
                            compute_background_amplitude, get_spot_parameter,
                            objective_function, fit_gaussian,
                            simulate_fitted_gaussian, gaussian_3d)
 
-_detection = ["detection", "compute_snr", "get_sigma", "detection_log_lm",
-              "detection_log_lm", "log_lm", "non_maximum_suppression_mask",
-              "from_threshold_to_spots", "from_threshold_to_snr"]
+_detection = ["log_lm", "local_maximum_detection", "spots_thresholding",
+              "compute_snr", "from_threshold_to_snr", "get_sigma",
+              "build_reference_spot"]
 
 _fit = ["get_spot_volume", "get_spot_surface", "build_grid",
         "compute_background_amplitude", "get_spot_parameter",
