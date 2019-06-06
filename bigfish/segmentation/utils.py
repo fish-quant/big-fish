bd140f74ce73e9e8d8346b868a04134fdef4fedb
Arthur Imbert <arthur.imbert.pro@gmail.com>
2019-06-06 14:11:30 +0200

    add 'get_boundaries'

--- bigfish/segmentation/utils.py
+++ bigfish/segmentation/utils.py
@@ -4,12 +4,17 @@
 Utilities function for nuclei and cytoplasm segmentation.
 """
 
+import warnings
+
 import bigfish.stack as stack
 
 import numpy as np
 from skimage.measure import label, regionprops
 from skimage.morphology import remove_small_objects
+from skimage.segmentation import find_boundaries
 
+
+# TODO homogenize the dtype of masks
 
 def label_instances(mask):
     """Count and label the different instances previously segmented in an
@@ -104,8 +109,10 @@ def merge_labels(label_1, label_2):
     nb_label_2 = label_2.max()
 
     # clean masks
-    label_1 = remove_small_objects(label_1, 3000)
-    label_2 = remove_small_objects(label_2, 3000)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        label_1 = remove_small_objects(label_1, 3000)
+        label_2 = remove_small_objects(label_2, 3000)
 
     # cast labels in np.int64
     label_1 = label_1.astype(np.int64)
@@ -122,3 +129,34 @@ def merge_labels(label_1, label_2):
     return label
 
 
+def get_boundaries(mask):
+    """Get the boundaries coordinates of a mask
+
+    Parameters
+    ----------
+    mask : np.ndarray, np.uint or np.int or bool
+        Labelled image with shape (y, x).
+
+    Returns
+    -------
+    boundaries : np.ndarray, np.int64
+        Coordinate of the boundaries with shape (nb_points, 2).
+
+    """
+    # check parameters
+    stack.check_array(mask,
+                      ndim=2,
+                      dtype=[np.uint8, np.uint16, np.int64, bool],
+                      allow_nan=False)
+
+    # get boundaries mask
+    boundary_mask = find_boundaries(mask, mode='inner')
+
+    # get peak coordinates and radius
+    boundary_coordinates = np.nonzero(boundary_mask)
+    boundary_coordinates = np.column_stack(boundary_coordinates)
+
+    # complete coordinates if necessary
+    boundary_coordinates = stack.complete_coordinates_2d(boundary_coordinates)
+
+    return boundary_coordinates
