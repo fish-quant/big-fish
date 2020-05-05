73aa119ca1e78e6733db0b6ed8be9058a27003dc
Arthur Imbert <arthur.imbert.pro@gmail.com>
2020-05-05 06:05:24 +0200

    refactor plots

--- bigfish/plot/__init__.py
+++ bigfish/plot/__init__.py
@@ -1,32 +1,43 @@
 # -*- coding: utf-8 -*-
+# Author: Arthur Imbert <arthur.imbert.pro@gmail.com>
+# License: BSD 3 clause
 
 """
-The bigfish.plot module includes function to plot images and simulated data.
+The bigfish.plot module includes functions to plot images and results.
 """
 
-from .plot_images import (plot_yx, plot_channels_2d, plot_segmentation,
-                          plot_images, plot_spot_detection,
-                          plot_illumination_surface,
-                          plot_segmentation_boundary, plot_foci_detection,
-                          plot_reference_spot)
-from .plot_coordinates import (plot_volume, plot_rna, plot_distribution_rna,
-                               plot_cell_coordinates, plot_layers_coordinates,
-                               plot_cell)
-from .plot_classification import plot_confusion_matrix, plot_2d_projection
+from .plot_images import plot_yx
+from .plot_images import plot_images
+from .plot_images import plot_segmentation
+from .plot_images import plot_segmentation_boundary
+from .plot_images import plot_detection
+from .plot_images import plot_spot_detection
+from .plot_images import plot_reference_spot
+from .plot_images import plot_foci_detection
+from .plot_images import plot_cell
+
 from .plot_quality import plot_sharpness
 
+from .plot_classification import plot_confusion_matrix
+from .plot_classification import plot_2d_projection
 
-_images = ["plot_yx", "plot_images", "plot_channels_2d",
-           "plot_illumination_surface", "plot_segmentation",
-           "plot_spot_detection", "plot_segmentation_boundary",
-           "plot_foci_detection", "plot_reference_spot"]
 
-_coordinates = ["plot_volume", "plot_rna", "plot_distribution_rna",
-                "plot_cell_coordinates", "plot_layers_coordinates",
-                "plot_cell"]
+_images = [
+    "plot_yx",
+    "plot_images",
+    "plot_segmentation",
+    "plot_segmentation_boundary",
+    "plot_detection",
+    "plot_spot_detection",
+    "plot_reference_spot",
+    "plot_foci_detection",
+    "plot_cell"]
 
-_classification = ["plot_confusion_matrix", "plot_2d_projection"]
+_classification = [
+    "plot_confusion_matrix",
+    "plot_2d_projection"]
 
-_quality = ["plot_sharpness"]
+_quality = [
+    "plot_sharpness"]
 
-__all__ = _images + _coordinates + _classification + _quality
+__all__ = _images + _classification + _quality
