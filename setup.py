5074dcabfbde23d1fae0e05297f8fc2fd2f800d4
Arthur Imbert <arthur.imbert.pro@gmail.com>
2020-05-13 21:06:46 +0200

    add version package

--- setup.py
+++ setup.py
@@ -6,46 +6,52 @@ Setup script.
 
 from setuptools import setup, find_packages
 
-# Package meta-data.
+# package description
 VERSION = "0.3.0"
-DESCRIPTION = 'Toolbox for the analysis of smFISH images.'
+DESCRIPTION = "Toolbox for the analysis of smFISH images."
 
-# Package abstract dependencies
-REQUIRES = [
-      'numpy >= 1.16.0',
-      'pip >= 18.1',
-      'scikit-learn >= 0.20.2',
-      'scikit-image >= 0.14.2',
-      'scipy >= 1.2.0',
-      'matplotlib >= 3.0.2',
-      'pandas >= 0.24.0',
-      'mrc >= 0.1.5'
-]
 
-DEEPLEARNING_REQUIREMENTS = ['tensorflow >= 1.12.0, < 2.0']
+# package version
+with open('bigfish/__init__.py') as f:
+    for row in f:
+        if row.startswith('__version__'):
+            VERSION = row.strip().split()[-1][1:-1]
+            break
 
-# Long description of the package
+# package abstract dependencies
+REQUIREMENTS = [
+    'numpy >= 1.16.0',
+    'pip >= 18.1',
+    'scikit-learn >= 0.20.2',
+    'scikit-image >= 0.14.2',
+    'scipy >= 1.2.0',
+    'matplotlib >= 3.0.2',
+    'pandas >= 0.24.0',
+    'mrc >= 0.1.5']
+DEEPLEARNING_REQUIREMENTS = [
+    'tensorflow >= 1.12.0, < 2.0']
+
+# long description of the package
 with open("README.md", "r") as f:
     LONG_DESCRIPTION = f.read()
 
 # A list of classifiers to categorize the project (only used for searching and
-# browsing projects on PyPI).
+# browsing projects on PyPI)
 CLASSIFIERS = [
-      'Development Status :: 2 - Beta',
-      'Intended Audience :: Science/Research',
-      'Intended Audience :: Developers',
-      'Intended Audience :: Biologist',
-      'Topic :: Software Development',
-      'Topic :: Scientific/Engineering',
-      'Topic :: Cellular Imagery',
-      'Operating System :: Unix',
-      'Operating System :: MacOS',
-      'Programming Language :: Python',
-      'Programming Language :: Python :: 3.6',
-      'License :: OSI Approved :: MIT License'
-]
+    'Development Status :: 2 - Beta',
+    'Intended Audience :: Science/Research',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Biologist',
+    'Topic :: Software Development',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Cellular Imagery',
+    'Operating System :: Unix',
+    'Operating System :: MacOS',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3.6',
+    'License :: OSI Approved :: MIT License']
 
-# Setup
+# setup
 setup(name='big-fish',
       version=VERSION,
       description=DESCRIPTION,
@@ -57,9 +63,6 @@ setup(name='big-fish',
       packages=find_packages(),
       license='MIT',
       python_requires='>=3.6.0',
-      install_requires=REQUIRES,
-      extras_require={
-            'deeplearning': DEEPLEARNING_REQUIREMENTS
-      },
-      classifiers=CLASSIFIERS
-      )
+      install_requires=REQUIREMENTS,
+      extras_require={'deeplearning': DEEPLEARNING_REQUIREMENTS},
+      classifiers=CLASSIFIERS)
