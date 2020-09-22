dba9aaa8acac60641ebca5595c74897c72af15cf
Arthur Imbert <arthur.imbert.pro@gmail.com>
2020-09-22 14:32:07 +0200

    update setup classifiers

--- setup.py
+++ setup.py
@@ -38,18 +38,20 @@ with open("README.md", "r") as f:
 # A list of classifiers to categorize the project (only used for searching and
 # browsing projects on PyPI)
 CLASSIFIERS = [
-    'Development Status :: 2 - Beta',
+    'Development Status :: 4 - Beta',
     'Intended Audience :: Science/Research',
     'Intended Audience :: Developers',
-    'Intended Audience :: Biologist',
     'Topic :: Software Development',
     'Topic :: Scientific/Engineering',
-    'Topic :: Cellular Imagery',
+    'Topic :: Scientific/Engineering :: Bio-Informatics',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'Topic :: Scientific/Engineering :: Image Processing',
+    'Topic :: Scientific/Engineering :: Image Recognition',
     'Operating System :: Unix',
     'Operating System :: MacOS',
     'Programming Language :: Python',
     'Programming Language :: Python :: 3.6',
-    'License :: OSI Approved :: MIT License']
+    'License :: OSI Approved :: BSD License']
 
 # setup
 setup(name='big-fish',
